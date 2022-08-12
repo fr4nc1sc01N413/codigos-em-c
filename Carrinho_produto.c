7a5c51b2b4efccd35005dfa2d8dee90b9c33ca83
ina1ecSc <f.inaie4191@gmail.com>
2022-08-12 14:03:42 -0300

    atualizando arquivo

--- Carrinho_produto.c
+++ Carrinho_produto.c
@@ -7,34 +7,47 @@ int main(void){
 	//pontuação final do time, e informar se o time está ou não rebaixado.
 	
 	//Primeiro criar uma opção de time para o usuario.
+	
 	int times
 	
 		printf("Bem-Vindo!\n");
+		
 		//aplicando os times que o usuario seleciona
-   		printf("Digite o numero que corresponde ao o time da serie A que deseja:\n");
-   		printf("(1)Ceara\n");
-   		printf("(2)Flamengo\n");
-   		printf("(3)Palmeiras\n");
-   		printf("(4)Atletico Mineiro\n");
-   		printf("(5)Corinthians\n");
-   		printf("(6)Athletico Paranaense\n");
-   		printf("(7)Fluminense\n");
-   		printf("(8)Botafogo\n");
-   		printf("(9)Fortaleza\n");
-   		printf("(10)juventude\n");
-   		printf("(11)Curitiba\n");
-   		printf("(12)Avai\n");
-   		printf("(13)Atletico Goaniense\n");
-   		printf("(14)Cuiaba\n");
-   		printf("(15)Goias\n");
-   		printf("(16)Internacional\n");
-   		printf("(17)Sao-Paulo\n");
-   		printf("(18)Bragantino\n");
-   		printf("(19)Santos\n");
-   		printf("(20)America-Mg\n");
-   		scanf("%d", &times);
-			
-			if
-	printf ("Ceara campeao da serie A");
+		
+   			printf("Digite o numero que corresponde ao o time da serie A que deseja:\n");
+   			printf("(1)Ceara\n");
+   			printf("(2)Flamengo\n");
+   			printf("(3)Palmeiras\n");
+   			printf("(4)Atletico Mineiro\n");
+   			printf("(5)Corinthians\n");
+   			printf("(6)Athletico Paranaense\n");
+   			printf("(7)Fluminense\n");
+   			printf("(8)Botafogo\n");
+   			printf("(9)Fortaleza\n");
+   			printf("(10)juventude\n");
+   			printf("(11)Curitiba\n");
+   			printf("(12)Avai\n");
+   			printf("(13)Atletico Goaniense\n");
+   			printf("(14)Cuiaba\n");
+   			printf("(15)Goias\n");
+   			printf("(16)Internacional\n");
+   			printf("(17)Sao-Paulo\n");
+   			printf("(18)Bragantino\n");
+   			printf("(19)Santos\n");
+   			printf("(20)America-Mg\n");
+   				scanf("%d", &times);
+   				
+				if(times = 0 || >= 21){
+					
+					printf("Indisponivel, escolha novamente\n");
+					
+				}
+				
+				if(times = 1){
+				
+					printf ("Ceara campeao da serie A");
+					
+				}
+				
 	return 0;
 }
